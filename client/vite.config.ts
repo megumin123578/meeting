7fb1a7312272b9ea1509d18cf823a8a2e2c74003
megumin123578 <saranghae123578@gmail.com>
2026-06-12 14:18:11 +0700

    update server.allowList

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:3001',
     },
-    allowedHosts:'meeting.n8nfmc.io.vn',
+    allowedHosts: ['meeting.n8nfmc.io.vn'],
   },
 })
